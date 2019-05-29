bb5bdeb2f8e7e4c596e88776f773843ce168209c
nhbala <nb378@cornell.edu>
2019-05-29 03:26:59 -0400

    more data processing

--- testing.py
+++ testing.py
@@ -8,6 +8,7 @@ from time import sleep
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
+from kneed import DataGenerator, KneeLocator
 
 
 PER_GAME_REG_OPTIMAL_K = 6
@@ -288,34 +289,118 @@ def creating_final_cat_Dicts():
     with open('datasets/regseasonavg_clusteringdict', 'w') as fp:
         json.dump(cat_dict, fp)
 
-json_object = json.load(open("datasets/post80salldata.json"))
-per_game_dict = {}
-for person in json_object:
-    person_stuff = json_object[person]
-    per_game_data = person_stuff["all_advanced"]
-    values = per_game_data[1:]
-    data_change_lst = [7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
-    for year_data in values:
-        games = year_data[5]
-        for column in data_change_lst:
-            if year_data[column] != "N/A":
-                year_data[column] = float(year_data[column]) * float(games)
-    career_per_game = [0] * len(per_game_data[1])
-    go_through_stats = per_game_data[1:]
-    divisors = [0] * len(per_game_data[1])
-    for index in range(len(go_through_stats)):
-        for index1 in range(len(go_through_stats[0])):
-            if index1 > 4:
-                if (go_through_stats[index][index1]) != "N/A":
-                    career_per_game[index1] += float(go_through_stats[index][index1])
-                    divisors[index1] += float(go_through_stats[index][5])
-    for index in range(len(career_per_game)):
-        if index > 6:
-            if divisors[index] != 0:
-                career_per_game[index] = career_per_game[index]/divisors[index]
-    per_game_dict[person] = [per_game_data[0]] + [career_per_game]
-with open('datasets/reg_season_advanced.json', 'w') as fp:
-    json.dump(per_game_dict, fp)
+def find_advanced_avg():
+    json_object = json.load(open("datasets/post80salldata.json"))
+    per_game_dict = {}
+    for person in json_object:
+        person_stuff = json_object[person]
+        per_game_data = person_stuff["all_advanced"]
+        values = per_game_data[1:]
+        data_change_lst = [7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
+        for year_data in values:
+            games = year_data[5]
+            for column in data_change_lst:
+                if year_data[column] != "N/A":
+                    year_data[column] = float(year_data[column]) * float(games)
+        career_per_game = [0] * len(per_game_data[1])
+        go_through_stats = per_game_data[1:]
+        divisors = [0] * len(per_game_data[1])
+        for index in range(len(go_through_stats)):
+            for index1 in range(len(go_through_stats[0])):
+                if index1 > 4:
+                    if (go_through_stats[index][index1]) != "N/A":
+                        career_per_game[index1] += float(go_through_stats[index][index1])
+                        divisors[index1] += float(go_through_stats[index][5])
+        for index in range(len(career_per_game)):
+            if index > 6:
+                if divisors[index] != 0:
+                    career_per_game[index] = career_per_game[index]/divisors[index]
+        per_game_dict[person] = [per_game_data[0]] + [career_per_game]
+    with open('datasets/reg_season_advanced.json', 'w') as fp:
+        json.dump(per_game_dict, fp)
 
-json_object = json.load(open("datasets/reg_season_advanced.json"))
-print(json_object["LeBron James"])
+def random_Crap():
+    json_object = json.load(open("datasets/reg_season_advanced.json"))
+    json_object_height = json.load(open("datasets/data.json"))
+    data = []
+    for person in json_object:
+        values = json_object[person]
+        values_to_add = (values[1])[5:]
+        first = (json_object_height[person])
+        json_acceptable_string = first.replace("'", "\"")
+        d = json.loads(json_acceptable_string)
+        curr_height = d['height']
+        height_array = curr_height.split('-')
+        final_height_inch = (int(height_array[0]) * 12) + int(height_array[1])
+        values_to_add.append(final_height_inch)
+        data.append(values_to_add)
+
+    mms = MinMaxScaler()
+    mms.fit(data)
+    data_transformed = mms.transform(data)
+
+    Sum_of_squared_distances = []
+    K = range(1,15)
+    for k in K:
+        km = KMeans(n_clusters=k)
+        km = km.fit(data_transformed)
+        Sum_of_squared_distances.append(km.inertia_)
+
+    kneedle = KneeLocator(K, Sum_of_squared_distances, S=1.0, curve='convex', direction='decreasing')
+    print(round(kneedle.knee, 3))
+
+def final_run():
+    json_object = json.load(open("datasets/reg_season_advanced.json"))
+    json_object1 = json.load(open("datasets/reg_season_per_game.json"))
+    json_object_height = json.load(open("datasets/data.json"))
+    data = []
+    reverse_dict = {}
+    for person in json_object:
+        values = json_object[person]
+        values1 = json_object1[person]
+        values_to_add = (values1[1])[5:]
+        gp = values_to_add[0]
+        gs = values_to_add[1]
+        start_rate = gs/gp
+        values_to_add[0] = 1 - start_rate
+        values_to_add[1] = start_rate
+        values_to_add1 = (values[1])[7:]
+        final_values = values_to_add + values_to_add1
+        first = (json_object_height[person])
+        json_acceptable_string = first.replace("'", "\"")
+        d = json.loads(json_acceptable_string)
+        curr_height = d['height']
+        height_array = curr_height.split('-')
+        final_height_inch = (int(height_array[0]) * 12) + int(height_array[1])
+        final_values.append(final_height_inch)
+        data.append(final_values)
+        reverse_dict[repr(final_values)] = person
+    km = KMeans(n_clusters=16, init='k-means++',
+                max_iter=100, n_init=10, verbose=0, random_state=34)
+    km = km.fit(data)
+    final_dict = {}
+    for index in range(len(km.labels_)):
+        curr_category = km.labels_[index]
+        curr_row = data[index]
+        curr_person = reverse_dict[repr(curr_row)]
+        (curr_row).append(curr_category)
+        curr_row = [float(i) for i in curr_row]
+        final_dict[curr_person] = curr_row
+    with open('datasets/with_cat_reg_season_advanced.json', 'w') as fp:
+        json.dump(final_dict, fp)
+
+    json_object = json.load(open("datasets/with_cat_reg_season_advanced.json"))
+    keys = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+    cat_dict = {key: [] for key in keys}
+    for person in json_object:
+        curr_person_data = json_object[person]
+        curr_category = (curr_person_data[-1])
+        curr_lst = cat_dict[curr_category]
+        curr_lst.append(person)
+        cat_dict[curr_category] = curr_lst
+
+    with open('datasets/final_data.json', 'w') as fp:
+        json.dump(cat_dict, fp)
+
+# json_object = json.load(open("datasets/reg_season_advanced.json"))
+# print(json_object["LeBron James"])
